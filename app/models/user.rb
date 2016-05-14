746de7c1d9ff382c0282adfc5679160b8bc07fbc
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-14 20:23:02 +0300

    Model User 'has_and_belongs_to_many' Chats.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
   validates :name, presence: true, uniqueness: true
+  has_and_belongs_to_many :chats, dependent: :destroy
 
 end
