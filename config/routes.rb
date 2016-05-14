77deae49827c61d8db0c68e57727d5f36407586b
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-14 20:58:48 +0300

    Added routes for chats.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users#, controllers: {sessions: "sessions"}
+  devise_for :users
   devise_scope :user do
     post 'user_sign_up' => 'registrations#create_user'
     post '/sessions' => 'sessions#create'
     delete '/sessions' => 'sessions#destroy'
   end
+
+  resources :chats
 
 end
