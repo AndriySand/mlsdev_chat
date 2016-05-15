70684685d1a67e9ef9697051cb393bada196eb46
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-15 17:30:34 +0300

    Refactored method 'find_and_add_participants'.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,16 +1,15 @@
 class Chat < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  has_and_belongs_to_many :participants, class_name: 'User', dependent: :destroy
+  has_and_belongs_to_many :participants, class_name: 'User'
   validates :participants, length: { minimum: 2, too_short: 'You should invite someone' }
 
   def find_and_add_participants(user_ids, current_user)
-    users = user_ids.nil? ? [] : user_ids.map{ |user_id| User.find_by_id(user_id) }
-    users.each{ |user| participants << user } if users.any?
-    participants << current_user
+    users = user_ids.nil? ? [current_user] : user_ids.uniq.map{ |id| User.find_by_id(id) } << current_user
+    users.each{ |user| participants << user unless participants.include?(user) }
   end
 
   def show_participant_ids
-    participants.map{|participant| participant.id}
+    participants.map(&:id)
   end
 
 end
