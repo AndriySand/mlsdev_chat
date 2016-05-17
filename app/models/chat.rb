10fcb41a23ad7b404d04680cfa8f8de0dd16bce8
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-17 13:07:36 +0300

    Chat model has_many :messages.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,6 +2,7 @@ class Chat < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :participants, class_name: 'User'
   validates :participants, length: { minimum: 2, too_short: 'You should invite someone' }
+  has_many :messages
 
   def find_and_add_participants(user_ids, current_user)
     users = user_ids.nil? ? [current_user] : user_ids.uniq.map{ |id| User.find_by_id(id) } << current_user
