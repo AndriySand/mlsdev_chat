b44dcc82dbe335b4ce67fc5728287e239a47469c
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-17 13:09:31 +0300

    Model User has_many :messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
          :recoverable, :validatable
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :chats
+  has_many :messages, foreign_key: 'author_id'
 
 end
