023647b5cc243d7ef0e8fba60a10899346949d4e
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-18 13:27:54 +0300

    Model Message has_many 'readings'.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,11 @@ class Message < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   belongs_to :chat
   validates :body, :author_id, :chat_id, presence: true
+  has_many :readings
+  after_save :ensure_reading_for_author
+
+  def ensure_reading_for_author
+  	readings.create(user_id: author_id)
+  end
 
 end
