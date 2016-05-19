8a6efb5c4ef8dcb259271e7390915e2325a6d179
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-19 15:01:47 +0300

    Added method 'index' into UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    @users = User.all
+    render_json_error('There are no users', :not_found) unless @users.any?
+  end
+
   def show
     @user = User.find_by_id(params[:id])
     render_json_error('There is no user under this ID', :not_found) unless @user
