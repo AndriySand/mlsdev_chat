3cefb04f0c5594e35c54344aad9986fa25cc6691
Andriy <andriy.samokhin@thinkmobiles.com>
2016-05-19 13:45:22 +0300

    Added 'index' method into MessagesController.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_chat, except: :show
 
   def create
-    chat = Chat.find(params[:chat_id])
     render_json_error('You are not authorized', :forbidden) and
-      return unless chat.authorized_user?(current_user)
-    @message = chat.messages.new(message_params)
+      return unless @chat.authorized_user?(current_user)
+    @message = @chat.messages.new(message_params)
     if @message.save
       render action: 'show', status: :created
     else
@@ -16,10 +16,19 @@ class MessagesController < ApplicationController
   def show
   end
 
+  def index
+    @messages = @chat.show_unread_messages(current_user)
+    render_json_error('There are no messages', :not_found) unless @messages.any?
+  end
+
   private
 
     def message_params
       params.require(:message).permit(:body, :author_id)
+    end
+
+    def find_chat
+      @chat = Chat.find(params[:chat_id])
     end
 
 end
